1a98aba4ef088e4037fbd69add546cd207b3916e
Rishen <57752048+Rishen-Gopaldass@users.noreply.github.com>
2020-12-30 09:58:44 +0200

    Update vendingMachine.py

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,22 +1,17 @@
 
 def Vendor():
 
-    # create product dictionary
     products = { 
     "A" : {"name": "coke" , "price": 20 },
     "B" : {"name": "fanta" , "price": 10 },
     "C" : {"name": "sprite" , "price": 25}  }
     print (products)
 
-    # product selection & extraction of price from dictionary
     while True:
         selection = input ( "\nEnter a product:\n").upper()
         price = int(products[selection]["price"])
         credit = 0
 
-        # asking user to insert money until the price is met.
-        # once met, goods are dispatched and a refund is calculated & paid out.
-        # option to make another purchase
         while (credit < price):
             credit = credit + (int(input("Please insert: R" + str(price - credit)+ "\n\n")))
         else:
